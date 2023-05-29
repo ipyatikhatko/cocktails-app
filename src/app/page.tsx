cfc6a3952b854f99f5684113e50c4315725f2f7f
Elijah <elijah.pyatikhatko@gmail.com>
2023-05-30 00:22:08 +0300

    fix responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,11 @@ export default async function Home() {
   const carouselMovies = await getCarouselMovies();
   const movieGenres = await getMovieGenres("en");
   return (
-    <main className="">
+    <main>
       <div className="flex flex-nowrap h-[70vh] w-screen">
         <PopularMoviesCarousel movies={carouselMovies.results.slice(0, 6)} />
       </div>
-      <section className="container mx-auto px-4 my-4">
+      <section className="mx-auto max-w-screen-xl px-4 my-4">
         <h1 className="w-fit text-xl text-blue-400">Discover movies</h1>
         <div className="flex items-center mb-4">
           <Suspense fallback={<div className="animate-pulse h-full rounded" />}>
--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -2,12 +2,12 @@
 
 import React from "react";
 import Image from "next/image";
-import { Movie } from "@/models/IMovie";
+import { IMovie } from "@/models/IMovie";
 import { imagePath } from "@/utils/imagePath";
 import clsx from "clsx";
 
 type MovieCardProps = {
-  movie: Movie;
+  movie: IMovie;
 };
 
 const MovieCard = ({ movie }: MovieCardProps) => {
@@ -24,11 +24,11 @@ const MovieCard = ({ movie }: MovieCardProps) => {
           className="transition-all filter brightness-100 group-hover:brightness-75 group-hover:scale-105 object-center object-cover"
           fill
           alt={movie.original_title}
-          src={imagePath(movie.poster_path, "w1280")}
+          src={imagePath(movie.poster_path, "w780")}
         />
       </div>
       <div className="flex flex-col">
-        <h3 className="text-sm text-black">{movie.title}</h3>
+        <h3 className="text-sm font-semibold text-black">{movie.title}</h3>
         <div className="flex gap-2 text-xs text-slate-600 font-thin">
           <span>{new Date(movie.release_date).getFullYear()}</span>
         </div>
