ada2dc67653814d8ac4d775abd854915aceeaef8
ipyatihatko <elijah.pyatikhatko@gmail.com>
2023-08-29 14:56:47 +0300

    refactor to swipable, skeleton

--- src/components/MoviesCarousel/MoviesCarouselItem.tsx
+++ src/components/MoviesCarousel/MoviesCarouselItem.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { MovieWithCredits } from "@/models/IMovie";
-import { Star } from "react-feather";
 import { imagePath } from "@/utils/imagePath";
 
 type Props = {
@@ -14,21 +13,20 @@ const MoviesCarouselItem = ({ movie, onImageLoad }: Props) => {
     onImageLoad();
   };
   return (
-    <div className="relative flex h-full w-full min-w-full min-h-full overflow-hidden">
+    <div className="relative flex h-full w-full flex-shrink-0 overflow-hidden">
       <div className="z-40 absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/50 to-transparent" />
-      <div className="absolute top-0 left-0 w-full h-full filter sm:brightness-50">
-        <Image
-          priority
-          draggable={false}
-          fill
-          alt="movie"
-          onLoad={handleImageLoad}
-          style={{
-            objectFit: "cover",
-          }}
-          src={imagePath(movie.backdrop_path, "w1280")}
-        />
-      </div>
+      <Image
+        priority
+        draggable={false}
+        fill
+        alt="movie"
+        onLoad={handleImageLoad}
+        style={{
+          filter: "blur(1px)",
+          objectFit: "cover",
+        }}
+        src={imagePath(movie.backdrop_path, "w1280")}
+      />
       <div className="p-4 sm:gap-4 flex flex-col mx-auto max-w-screen-xl h-fit">
         <h1 className="mb-4 z-50 text-sm sm:text-2xl font-bold text-white">
           {movie.title}
@@ -40,7 +38,7 @@ const MoviesCarouselItem = ({ movie, onImageLoad }: Props) => {
               draggable={false}
               width={320}
               height={640}
-              className="w-[280px] sm:h-[400px] md:h-[470px] md:w-[320px]"
+              className="w-[280px] h-auto sm:w-auto sm:h-[400px] md:h-[470px]"
               alt="poster"
               style={{
                 objectFit: "cover",
@@ -48,7 +46,7 @@ const MoviesCarouselItem = ({ movie, onImageLoad }: Props) => {
               src={imagePath(movie.poster_path, "w780")}
             />
           </div>
-          <div className="hidden z-50 w-full h-full sm:w-2/3 xs:flex items-end justify-end p-4 bg-black/50">
+          <div className="hidden ml-2 z-50 w-full h-full sm:w-2/3 xs:flex items-end justify-end p-4 bg-black/50">
             <p className="text-sm lg:text-xl font-thin text-white leading-loose line-clamp-4 md:line-clamp-13">
               {movie.overview}
             </p>
--- src/components/MoviesCarousel/index.tsx
+++ src/components/MoviesCarousel/index.tsx
@@ -1,80 +1,75 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import clsx from "clsx";
 import MoviesCarouselItem from "./MoviesCarouselItem";
 import { MovieWithCredits } from "@/models/IMovie";
-import clsx from "clsx";
-import { Play, Pause } from "react-feather";
+import { ArrowLeft, ArrowRight } from "react-feather";
+import SwipeableViews, { OnSwitchingCallback } from "react-swipeable-views";
+import MoviesCarouselSkeleton from "./MoviesCarouselSkeleton";
 
 type Props = {
   movies: MovieWithCredits[];
   containerClassName?: string;
+  autoplayInterval?: number;
 };
 
-const MoviesCarousel = ({ movies, containerClassName }: Props) => {
-  const containerRef = useRef<HTMLDivElement>(null);
+const MoviesCarousel = ({
+  movies,
+  containerClassName,
+  autoplayInterval = 8000,
+}: Props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [selectedSlide, setSelectedSlide] = useState(0);
-  const [requestedByUser, setRequestedByUser] = useState(false);
-  const [loadedImagesCount, setLoadedImagesCount] = useState(0); // Track loaded images count
+  const [imagesLoadedCount, setImagesLoadedCount] = useState(0);
+  const autoPlayIntervalRef = useRef<NodeJS.Timer | undefined>(undefined);
 
-  // Callback function to handle image load notification
-  const handleImageLoad = () => {
-    setLoadedImagesCount((prevCount) => prevCount + 1);
+  const allImagesLoaded = imagesLoadedCount === movies.length;
+
+  const handleImageLoaded = () => setImagesLoadedCount((prev) => prev + 1);
+  const handleChangeIndexManually = (index: number) => {
+    clearTimeout(autoPlayIntervalRef.current);
+    setCurrentIndex(index);
+    autoPlayIntervalRef.current = setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % movies.length);
+    }, autoplayInterval);
   };
 
-  const handleSelectItem = (event: React.MouseEvent, index: number) => {
-    event.stopPropagation();
-    setSelectedSlide(index);
-    setRequestedByUser(true);
+  const handleSwitching: OnSwitchingCallback = (index, type) => {
+    if (type != "end") return;
+    if (index === movies.length - 1) {
+      handleChangeIndexManually(0);
+    }
   };
 
   useEffect(() => {
-    const container = containerRef.current;
-    if (!container) return;
+    if (!allImagesLoaded) return;
+    autoPlayIntervalRef.current = setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % movies.length);
+    }, autoplayInterval);
 
-    const scrollCarousel = () => {
-      if (!requestedByUser) {
-        let nextIndex = (currentIndex + 1) % movies.length;
-        let nextPosition = nextIndex * container.offsetWidth;
+    return () => clearTimeout(autoPlayIntervalRef.current);
+  }, [autoplayInterval, movies.length, allImagesLoaded]);
 
-        setCurrentIndex(nextIndex);
-        container.scrollTo({
-          left: nextPosition,
-          behavior: "smooth",
-        });
-      } else if (requestedByUser) {
-        let nextPosition = selectedSlide * container.offsetWidth;
+  const handlePrevClick = () => {
+    handleChangeIndexManually(
+      currentIndex === 0 ? movies.length - 1 : currentIndex - 1
+    );
+  };
 
-        setCurrentIndex(selectedSlide);
-        container.scrollTo({
-          left: nextPosition,
-          behavior: "smooth",
-        });
-      }
-      setRequestedByUser(false);
-    };
-
-    const timeout = setTimeout(scrollCarousel, requestedByUser ? 200 : 10000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [currentIndex, selectedSlide, requestedByUser, movies.length]);
-
-  const allImagesLoaded = loadedImagesCount === movies.length;
+  const handleNextClick = () => {
+    handleChangeIndexManually((currentIndex + 1) % movies.length);
+  };
 
   return (
     <div className={clsx("flex flex-col relative overflow-hidden")}>
-      {!allImagesLoaded && (
-        <div className="z-50 absolute top-0 left-0 w-full h-full bg-gray-300 animate-pulse grid place-items-center">
-          <h3 className="text-center">Loading carousel</h3>
-        </div>
-      )}
-      <div
-        ref={containerRef}
+      {!allImagesLoaded && <MoviesCarouselSkeleton />}
+      <SwipeableViews
+        index={currentIndex}
+        onChangeIndex={handleChangeIndexManually}
+        onSwitching={handleSwitching}
+        enableMouseEvents
         className={clsx(
-          "flex h-full w-full overflow-hidden transition-opacity duration-500",
-          !allImagesLoaded ? "opacity-0" : "opacity-100",
+          "transition-transform duration-300",
+          allImagesLoaded ? "opacity-100" : "opacity-0",
           containerClassName
         )}
       >
@@ -82,28 +77,18 @@ const MoviesCarousel = ({ movies, containerClassName }: Props) => {
           <MoviesCarouselItem
             key={movie.id}
             movie={movie}
-            onImageLoad={handleImageLoad}
+            onImageLoad={handleImageLoaded}
           />
         ))}
-      </div>
-      <div className="flex w-full justify-center items-center">
-        <div className="rounded-full flex gap-4 lg:gap-2 items-center h-8 w-fit">
-          {movies.map((m, i) => (
-            <div
-              className="group grid place-items-center h-full cursor-pointer z-50"
-              key={m.id}
-              onClick={(e) => handleSelectItem(e, i)}
-            >
-              <div
-                className={clsx(
-                  "w-8 h-2 lg:h-1 bg-slate-200 rounded group-hover:bg-opacity-60",
-                  "transition-all",
-                  currentIndex === i && "bg-slate-400"
-                )}
-              />
-            </div>
-          ))}
-        </div>
+      </SwipeableViews>
+      <div
+        className={clsx(
+          "flex w-full justify-center items-center mt-2",
+          !allImagesLoaded ? "hidden" : "block"
+        )}
+      >
+        <ArrowLeft className="cursor-pointer" onClick={handlePrevClick} />
+        <ArrowRight className="cursor-pointer ml-4" onClick={handleNextClick} />
       </div>
     </div>
   );
